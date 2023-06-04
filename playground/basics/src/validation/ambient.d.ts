f1dc60885837875d26a2887fbc11ad6588543b45
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-04 10:54:21 +0300

    refactor: remove declare module "./$form/*" types

--- playground/basics/src/validation/ambient.d.ts
+++ playground/basics/src/validation/ambient.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="ajv-build-tools/types" />
-
-declare module "./$form/*" {
-    import { GeneratedCreateValidate } from "kitva/forms/client";
-    export const createValidate: GeneratedCreateValidate;
-}
