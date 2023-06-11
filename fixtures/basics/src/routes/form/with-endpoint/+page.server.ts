07fcb55c2b8fe7743a56d6c54e2f783c5200df9d
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-11 19:53:13 +0300

    Use new $types2

--- fixtures/basics/src/routes/form/with-endpoint/+page.server.ts
+++ fixtures/basics/src/routes/form/with-endpoint/+page.server.ts
@@ -1,6 +1,6 @@
 import { withValidation } from "kitva/forms/server";
-import type { Actions } from "./$types";
 import { fail } from "@sveltejs/kit";
+import type { Actions } from "./$types2";
 
 export const actions: Actions = withValidation({
     default(event) {
