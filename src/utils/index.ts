c0ed3b4ab5fd07e6f1b07c54d5d4a0ab9bd693c9
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-12 15:31:57 +0300

    logger first param to support %s format string

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,6 +10,12 @@ export const HTTP_PARTS = ["body", "headers", "queries", "params"] as const;
 
 export const createDebug = (ns: string) => createDebugger(`kitva:${ns}`);
 
-export const warn = (...args: any) => console.warn(bold(yellow("warn:")), ...args);
-export const error = (...args: any) => console.error(bold(red("ERROR:")), ...args);
-export const info = (...args: any) => console.log(bold(red("info:")), ...args);
+export const warn = (msg: string, ...args: any) => {
+    console.warn(`${bold(yellow("warn:"))} ${msg}`, ...args);
+};
+export const error = (msg: string, ...args: any) => {
+    console.error(`${bold(red("ERROR:"))} ${msg}`, ...args);
+};
+export const info = (msg: string, ...args: any) => {
+    console.log(`${bold(red("info:"))} ${msg}`, ...args);
+};
