0033a1dd88632bb4b99f802ee853d3878fc94b77
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-16 00:22:28 +0300

    feat(cli): add ambient.d.ts for virtual files

--- src/cli/add_types.ts
+++ src/cli/add_types.ts
@@ -6,25 +6,27 @@ const type_defs = `import "kitva/ambient";`;
 const locals_def = 'validation: import("kitva/presets/ajv/index").AppLocal';
 
 export async function addTypes(cwd: string) {
-    const app_d_ts = resolve(cwd, "src/app.d.ts");
-    const content = await readFile(app_d_ts, "utf8");
+	const app_d_ts = resolve(cwd, "src/app.d.ts");
+	const content = await readFile(app_d_ts, "utf8");
 
-    return writeFile(app_d_ts, editAppDts(content));
+	return writeFile(app_d_ts, editAppDts(content));
 }
 
 export function editAppDts(content: string) {
-    let new_content = content;
-    if (!content.includes(type_defs)) {
-        new_content = `${type_defs}\n${content}`;
-    }
-    if (!content.includes(locals_def)) {
-        new_content = new_content.replace(
-            /(\/\/)* (interface Locals {)/,
-            (_, __, intrfc) => {
-                return `${intrfc}\n\t${locals_def};\n\t`;
-            }
-        );
-    }
+	let new_content = content;
+	if (!content.includes(type_defs)) {
+		new_content = `${type_defs}\n${content}`;
+	}
 
-    return new_content;
+	// this will be handled by a vite plugin to generate $types2 with all locals type setup to each route
+	// if (!content.includes(locals_def)) {
+	//     new_content = new_content.replace(
+	//         /(\/\/)* (interface Locals {)/,
+	//         (_, __, intrfc) => {
+	//             return `${intrfc}\n\t${locals_def};\n\t`;
+	//         }
+	//     );
+	// }
+
+	return new_content;
 }
--- src/cli/setup.ts
+++ src/cli/setup.ts
@@ -5,55 +5,56 @@ import { addTsRootDir } from "./edit_tsconfig.js";
 import { addValidationHook } from "./add_hook.js";
 import { warn } from "../utils/index.js";
 import { bold, green } from "kleur/colors";
+import { addTypes } from "./add_types.js";
 
 function getViteConfig(cwd: string) {
-    const vite_js = resolve(cwd, "vite.config.js");
-    if (existsSync(vite_js)) {
-        return vite_js;
-    }
+	const vite_js = resolve(cwd, "vite.config.js");
+	if (existsSync(vite_js)) {
+		return vite_js;
+	}
 
-    const vite_ts = resolve(cwd, "vite.config.ts");
-    if (existsSync(vite_ts)) {
-        return vite_ts;
-    }
+	const vite_ts = resolve(cwd, "vite.config.ts");
+	if (existsSync(vite_ts)) {
+		return vite_ts;
+	}
 }
 
 export async function setup(cwd: string) {
-    console.log(bold("Setting up Kitva...."));
+	console.log(bold("Setting up Kitva...."));
 
-    const r = (p: string) => resolve(cwd, p);
+	const r = (p: string) => resolve(cwd, p);
 
-    const vite_config = getViteConfig(cwd);
-    const is_svelte_project = existsSync(r("svelte.config.js"));
-    const tsconfig_path = r("tsconfig.json");
-    const jsconfig_path = r("jsconfig.json");
-    const is_ts_project = existsSync(tsconfig_path);
-    const is_js_project = existsSync(jsconfig_path);
+	const vite_config = getViteConfig(cwd);
+	const is_svelte_project = existsSync(r("svelte.config.js"));
+	const tsconfig_path = r("tsconfig.json");
+	const jsconfig_path = r("jsconfig.json");
+	const is_ts_project = existsSync(tsconfig_path);
+	const is_js_project = existsSync(jsconfig_path);
 
-    const tsconfig = is_ts_project ? tsconfig_path : is_js_project && jsconfig_path;
+	const tsconfig = is_ts_project ? tsconfig_path : is_js_project && jsconfig_path;
 
-    if (!is_svelte_project) {
-        throw new Error(
-            "svelte.config.js is not found. please make sure to run inside a sveltekit project"
-        );
-    }
+	if (!is_svelte_project) {
+		throw new Error(
+			"svelte.config.js is not found. please make sure to run inside a sveltekit project"
+		);
+	}
 
-    if (!vite_config) {
-        throw new Error("vite.config is not found");
-    }
+	if (!vite_config) {
+		throw new Error("vite.config is not found");
+	}
 
-    const ext = is_ts_project ? ".ts" : ".js";
+	const ext = is_ts_project ? ".ts" : ".js";
 
-    await addVitePlugin(vite_config);
+	await addVitePlugin(vite_config);
 
-    await addValidationHook(cwd, ext);
+	await addValidationHook(cwd, ext);
 
-    if (tsconfig) {
-        // await addTypes(cwd);
-        await addTsRootDir(tsconfig);
-    } else {
-        warn("Could not add setup types because tsconfig/jsconfig is missing");
-    }
+	if (tsconfig) {
+		await addTypes(cwd);
+		await addTsRootDir(tsconfig);
+	} else {
+		warn("Could not add setup types because tsconfig/jsconfig is missing");
+	}
 
-    console.log(green("Setup done"), "Make sure to install kitva package");
+	console.log(green("Setup done"), "Make sure to install kitva package");
 }
