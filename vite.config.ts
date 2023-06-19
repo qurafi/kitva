8193d525fb372753faaef8926b1639c10718874b
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-19 03:22:58 +0300

    chore: move vitest config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		// singleThread: true,
+		threads: true,
+		include: ["src/**/*.{test,spec}.{js,ts}"]
+	}
 });
--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-	test: {
-		// singleThread: true,
-		threads: true,
-		include: ["{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"]
-	}
-});
