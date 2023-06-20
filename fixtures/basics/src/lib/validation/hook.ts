a6eb693d2ea639dfdb31c9c137438c01e2b27b4c
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-20 18:47:18 +0300

    fix: missing "./hook/index" specifier

--- fixtures/basics/src/lib/validation/hook.ts
+++ fixtures/basics/src/lib/validation/hook.ts
@@ -1,6 +1,6 @@
 import schemas from "$schemas?t=all";
 
-import { validationHook as getValidationHook } from "kitva/hook/index";
+import { validationHook as getValidationHook } from "kitva/hook";
 import { createPreset } from "kitva/presets/ajv/server";
 
 export const preset = createPreset(schemas);
