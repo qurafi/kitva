976e8de813bde5e58e0e80b00e0695f9be66322c
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2024-03-19 04:00:29 +0300

    tests: add firefox during build-time tests

--- fixtures/basics/playwright.config.ts
+++ fixtures/basics/playwright.config.ts
@@ -1,14 +1,28 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
 
 const use_build = process.env.CI || process.env.TEST_MODE == "build";
-const config: PlaywrightTestConfig = {
+const config = {
 	webServer: {
 		command: use_build ? "npm run build && npm run preview" : "pnpm dev",
 		port: use_build ? 4173 : 8180,
 		stdout: "pipe"
 	},
 	testDir: "tests",
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
-};
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] }
+		}
+	]
+} satisfies PlaywrightTestConfig;
+
+if (use_build) {
+	config.projects?.push({
+		name: "firefox",
+		use: { ...devices["Desktop Firefox"] }
+	});
+}
 
 export default config;
