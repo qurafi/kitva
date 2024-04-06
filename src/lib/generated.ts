9713db756470e4e61e7806ff8124ff48e6e46acd
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2024-04-06 20:43:30 +0300

    remove RewriteHandlers type

--- src/lib/generated.ts
+++ src/lib/generated.ts
@@ -1,6 +1,6 @@
 import type { HttpMethod, HttpPart } from "$lib/shared/constants.js";
 import type { AjvError } from "./runtime/ajv/index.js";
-import type { AnyHandler, RequestHandlerWithValidation, AnyActions } from "./types.js";
+import type { RequestHandlerWithValidation, AnyActions, EventWithValidation } from "./types.js";
 
 export type { FormResult, FormResults } from "./types/forms.js";
 export type { AjvError, HttpMethod, HttpPart, RequestHandlerWithValidation };
@@ -8,16 +8,10 @@ export type { AjvError, HttpMethod, HttpPart, RequestHandlerWithValidation };
 export { defineGenerated } from "./runtime/client/client.js";
 export { withValidation } from "./runtime/server/forms.js";
 
+export type { EventWithValidation };
+
 export type RouteSchemas = Partial<Record<HttpMethod, Partial<Record<HttpPart, any>>>> & {
 	actions?: Record<string, any>;
-};
-
-export type RewriteHandlers<Schemas extends RouteSchemas, Handler extends AnyHandler> = {
-	[k in HttpMethod]: k extends keyof Schemas
-		? Schemas[k] extends NonNullable<unknown>
-			? RequestHandlerWithValidation<Handler, Schemas[k], true>
-			: never
-		: never;
 };
 
 export type RewriteActions<ActionsData extends Record<string, any>, Handlers extends AnyActions> = {
